7bf8645acfb236b1b4f1141fb011baf4d6aa29a1
Grey Altr <grey.sifr@gmail.com>
2022-11-17 12:29:13 -0800

    correction to movies-container element

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 /* Imports */
 import { fetchMovies } from './fetch-utils.js';
+import { renderMovies } from './render-utils.js';
 /* Get DOM Elements */
-
+const moviesContainer = document.getElementById('movies-container');
 /* State */
 
 /* Events */
 
 /* Display Functions */
-
+window.addEventListener('load', async () => {
+    const movies = await fetchMovies();
+    for (let movie of movies) {
+        const movieEl = renderMovies(movie);
+        moviesContainer.append(movieEl);
+    }
+});
 // (don't forget to call any display functions you want to run on page load!)
