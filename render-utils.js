6f49cfb479373f58b553d93d11992cb8d31d940f
Grey Altr <grey.sifr@gmail.com>
2022-11-17 13:07:01 -0800

    refactored genre element import and display from an array

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,20 @@
 export function renderMovies(movies) {
     const div = document.createElement('div');
-    div.classList.add('movie-card');
+    div.classList.add('movies-card');
 
     const titleEl = document.createElement('h3');
     titleEl.textContent = movies.title;
 
+    const ul = document.createElement('ul');
+    for (let genre of movies.genre) {
+        const li = document.createElement('li');
+        li.textContent = genre;
+        ul.append(li);
+    }
+
     const genreEl = document.createElement('p');
-    genreEl.textContent = movies.genre;
+    genreEl.textContent = 'Genre:';
+    genreEl.append(ul);
 
     const locEl = document.createElement('p');
     locEl.textContent = movies.localization;
