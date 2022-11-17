6cb2f0272607ddb00f1e4d4d29399f346958c8ca
Grey Altr <grey.sifr@gmail.com>
2022-11-17 11:14:23 -0800

    added CDN script to index.html, and created fetch-utils.js

---
+++ fetch-utils.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://pwsxvrnlhooihwcxftvf.supabase.co';
+const SUBABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3c3h2cm5saG9vaWh3Y3hmdHZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDg2MzQsImV4cCI6MTk4MzY4NDYzNH0.ITSI6KK7aGizRAaYZ1zCgnyVWSXCZKMfeSCKISd7IXg';
