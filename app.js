e836c3dfdcd237ee1b9b4199736a52f456948f06
Grey Altr <grey.sifr@gmail.com>
2022-11-18 16:55:23 -0800

    created eventListener for cities in app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /* Imports */
-import { fetchMovies } from './fetch-utils.js';
+import { fetchMovies, fetchCities } from './fetch-utils.js';
 import { renderMovies } from './render-utils.js';
 /* Get DOM Elements */
 const moviesContainer = document.getElementById('movies-container');
+const citiesContainer = document.getElementById('cities-container');
 /* State */
 
 /* Events */
@@ -13,6 +14,14 @@ window.addEventListener('load', async () => {
     for (let movie of movies) {
         const movieEl = renderMovies(movie);
         moviesContainer.append(movieEl);
+    }
+});
+
+window.addEventListener('load', async () => {
+    const cities = await fetchCities();
+    for (let city of cities) {
+        const cityEl = renderMovies(city);
+        moviesContainer.append(cityEl);
     }
 });
 // (don't forget to call any display functions you want to run on page load!)
